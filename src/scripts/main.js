c36b5640c3d67c16ac694142ab72f855e29467ae
jawc-05 <joaoalfredowc@gmail.com>
2025-06-01 12:50:49 -0300

    added the countdown to the event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dateEvent = new Date("Dec 12, 2025, 19:00:00");
+const timeStampEvent = dateEvent.getTime();
+
+
+const countdown = setInterval(function(){
+    const now = new Date();
+    const timeStampNow = now.getTime();
+
+    const timeLeft = timeStampEvent - timeStampNow;
+
+    const daysInMs = 1000 * 60 * 60 * 24;
+    const hoursInMs = 1000 * 60 * 60;
+    const minutesInMs = 1000 * 60;
+
+    const daysUntilEvent = Math.floor(timeLeft / daysInMs);
+    const hoursUntilEvent = Math.floor((timeLeft % daysInMs) / hoursInMs);
+    const minutesUntilEvent = Math.floor((timeLeft % hoursInMs) / minutesInMs);
+    const secondsUntilEvent = Math.floor((timeLeft % minutesInMs) / 1000);
+
+    document.getElementById('timer').innerHTML = ` começa em ${daysUntilEvent}d ${hoursUntilEvent}h ${minutesUntilEvent}m ${secondsUntilEvent}s`;
+
+    if (timeLeft < 0){
+        clearInterval(countdown);
+        document.getElementById('timer').innerHTML = " já aconteceu!";
+    }
+}, 1000);
